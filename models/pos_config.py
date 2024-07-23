b068c2f470cbdfe73d08a2f0a9dc7da4a89e8c43
Ariana Carrillo <ariana@computadora911.com>
2024-07-23 18:51:22 +0000

    Cambios para pago credomatic puntos

--- models/pos_config.py
+++ models/pos_config.py
@@ -3,4 +3,9 @@ from odoo import models, fields
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    terminal_id = fields.Char("Terminal ID", store=True)
+    terminal_id = fields.Char("Terminal ID para cobro automático", store=True)
+    terminal_puntos_id = fields.Char("Terminal ID para cobro con puntos", store=True)
+    equivalencia_puntos = fields.Integer("Cantidad de puntos por quetzal", store=True, help="Cantidad de puntos por Q1")
+    cantidad_minima_puntos = fields.Monetary("Venta mínima en quetzales para canje de puntos", currency_field="company_currency_id", store=True)
+    company_currency_id = fields.Many2one("res.currency", related="company_id.currency_id", string="Moneda", readonly=True, store=True)
+
