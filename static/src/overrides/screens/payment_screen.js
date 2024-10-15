078501527138efae6a11a91f87c2a43f08b8232b
Ariana Carrillo <ariana@computadora911.com>
2024-10-15 14:49:02 +0000

    Consulta de puntos en pos

--- static/src/overrides/screens/payment_screen.js
+++ static/src/overrides/screens/payment_screen.js
@@ -1,13 +1,8 @@
 /** @odoo-module **/
-
 import { _t } from "@web/core/l10n/translation";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
-
 import { patch } from "@web/core/utils/patch";
 import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
-import { useService } from "@web/core/utils/hooks";
-import { SelectionPopup } from "@point_of_sale/app/utils/input_popups/selection_popup";
-import { AnulacionReceipt } from "@pos_credomatic/app/anulacion_receipt";
 
 patch(PaymentScreen.prototype, {
     async consultarPuntos() {
@@ -15,7 +10,6 @@ patch(PaymentScreen.prototype, {
         var inquiry_data = "terminalId:"+this.pos.config.terminal_puntos_id+";transactionType:POINTS_INQUIRY;invoice:"+this.currentOrder.uid.replaceAll("-", "")+";pointsPlan:00";
         console.info(inquiry_data)
         var response = service.SdkInvoke(inquiry_data);
-        response = '{"acqNumber":"","cardHolderName":"ARIANA CARRILLO","salesAmount":"000000050000","authorizationNumber":"0158533232329","cardBrand":"retail EMV","hostDate":"09232024","hostTime":"171346","invoice":"undefined","maskedCardNumber":"448796XXXXXX0125","referenceNumber":"17015859","responseCode":"00","responseCodeDescription":"APROBADA","systemTraceNumber":"067836","transactionId":"06783517015859050158","currencyVoucher":"GTQ","TerminalDisplayLine1Voucher":"TEST ISC480","TerminalDisplayLine2Voucher":"PRUEBAS INTEGRACUONED BAC","TerminalDisplayLine3Voucher":"ZONA 12","trnTotalTime":"37429"}1234'
 
         try {
             console.log('response', response)
--- static/src/overrides/screens/ticket_screen.js
+++ static/src/overrides/screens/ticket_screen.js
@@ -1,5 +1,4 @@
 /** @odoo-module **/
-
 import { _t } from "@web/core/l10n/translation";
 import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
 import { patch } from "@web/core/utils/patch";
