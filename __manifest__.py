0a7e83d6c8ca6d4a230be3b11d30faabcdd13ce7
Ariana Carrillo <ariana@computadora911.com>
2025-05-15 10:32:26 +0000

    cambio de version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     'name': 'POS Credomátic',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'Custom',
     'description': """ POS Credomátic """,
     'author': 'aquíH',
